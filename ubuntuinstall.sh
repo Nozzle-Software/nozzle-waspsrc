f29d17b496a66baf07f251251dbf2aed1b09761d
Aaha3-1 <Aaha3.sh@gmail.com>
2025-08-17 15:02:21 +1000

    Create ubuntuinstall.sh because i lost faith in humanity

---
+++ ubuntuinstall.sh
@@ -0,0 +1,7 @@
+sudo dpkg --add-architecture i386 && sudo apt update
+
+sudo apt update && sudo apt install \
+  gcc-multilib g++-multilib libpng-dev \
+  libx11-dev:i386 libxext-dev:i386
+
+# See? how simple. Next time, use archlinux!
