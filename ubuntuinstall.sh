47f81fed88fd26fd6fe2c596ac35170f048d440f
Aaha3-1 <Aaha3.sh@gmail.com>
2025-08-30 19:49:15 +1000

    why do i bother updating this file?

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -2,6 +2,5 @@ sudo dpkg --add-architecture i386 && sudo apt update
 
 sudo apt update && sudo apt install \
   gcc-multilib g++-multilib libpng-dev \
-  libx11-dev:i386 libxext-dev:i386
-
-# See? how simple. Next time, use archlinux!
+  libx11-dev:i386 libxext-dev:i386 \
+  python3 scons \
